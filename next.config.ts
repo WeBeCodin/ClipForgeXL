b4319943cfe3b4c699d5e55a9b85b5eef59599c7
Cooking With Pwn Juice <pwnjuicethechef@gmail.com>
2025-07-16 14:38:33 +0000

    This error means your Firebase client SDK is not properly initialized—th

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type {NextConfig} from 'next';
-import('dotenv').then((dotenv) => dotenv.config());
 
 const nextConfig: NextConfig = {
   /* config options here */
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,11 +6,11 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "clipforge-xl.firebaseapp.com",
-  projectId: "clipforge-xl",
-  storageBucket: "clipforge-xl.appspot.com",
-  messagingSenderId: "838499848588",
-  appId: "1:838499848588:web:49a12e55ddb94cf14246a9"
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
